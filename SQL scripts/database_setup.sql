75c26ba022d5d32876af171294eeffacc38e85ff
Hallelujah5 <156408625+Hallelujah5@users.noreply.github.com>
2024-03-27 23:28:15 +0700

    Update database_setup.sql
    user ==> eoi

--- SQL scripts/database_setup.sql
+++ SQL scripts/database_setup.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE  IF NOT EXISTS company_php;
 
--- Create user
+-- Create eoi
 -- CREATE USER IF NOT exists 'assignment2_inquiry'@'localhost' IDENTIFIED BY 'admin';
 -- GRANT ALL PRIVILEGES ON company_php.* TO 'assignment2_inquiry'@'localhost';
 -- FLUSH PRIVILEGES;
 -- drop user 'assigment2_inquiry'@'localhost';
 
 drop table skills;
-drop table users;
+drop table eoi;
 
 
 -- Create table
 use company_php;
 
-CREATE TABLE IF NOT exists users (
+CREATE TABLE IF NOT exists eoi (
   user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   job_reference_number CHAR(5) DEFAULT NULL,
   first_name VARCHAR(20) NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT exists users (
   postcode INT(4) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   phone_number VARCHAR(12) NOT NULL,
-  status BIT(1) DEFAULT 1,
+  status ENUM('New', 'Current', 'Final') DEFAULT 'New',
   role BIT(1) DEFAULT 0
 );
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT exists skills (
 -- 0 male, 1 female
 -- 0 user, 1 manager
 -- Sample
-INSERT INTO users (first_name, last_name, date_of_birth, gender, street_address, suburb_town, state, postcode, email, phone_number, role, password)
+INSERT INTO eoi (first_name, last_name, date_of_birth, gender, street_address, suburb_town, state, postcode, email, phone_number, role, password)
 VALUES ('John', 'Doe', '1990-01-01', 0, '123 Main Street', 'Anytown', 'NSW', 1234, 'johndoe@example.com', '0412345678', 0, 'password123');
 
-select * from users;
+select * from eoi;
