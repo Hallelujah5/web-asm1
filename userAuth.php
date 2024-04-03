3a468112fb86a95b663186025e442cd9ed349d1d
Hallelujah5 <156408625+Hallelujah5@users.noreply.github.com>
2024-04-03 23:48:27 +0700

    Update userAuth.php

--- userAuth.php
+++ userAuth.php
@@ -4,7 +4,7 @@ session_start();
 
 
 
-
+//Checks if email and password is exist, and isn't empty, then assign it to $ variables.
 if(isset($_POST["email"]) && $_POST["email"] != "" && isset($_POST["password"]) && $_POST["password"] != "") {
     $email = $_POST["email"];
     $password = $_POST["password"];
@@ -20,19 +20,19 @@ if(isset($_POST["email"]) && $_POST["email"] != "" && isset($_POST["password"]) 
 
 function authenticateUser($email, $loginPassword) {
     include("dbConnect.php");
-    // $hashedPassword = password_hash($loginPassword, PASSWORD_DEFAULT);
 
     //Create a prepare statment to prevent SQL injection
-    $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ? AND password = ?");
-    mysqli_stmt_bind_param($stmt, "ss", $email, $loginPassword);
-    mysqli_stmt_execute($stmt);
+    $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ? AND password = ?");  // '?' here means the user's input, it wants to query the column where email and password is exactly the user's input.
+    mysqli_stmt_bind_param($stmt, "ss", $email, $loginPassword);        // 'ss' = two strings.
+    mysqli_stmt_execute($stmt);            //we used bind and execute statement functions for extra security.
     $result = mysqli_stmt_get_result($stmt);
 
+    //save server resources.
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
 
 
-    if (mysqli_num_rows($result) == 1) {
+    if (mysqli_num_rows($result) == 1) {        //if returns a row, which means it found a user, then assign it to the session email and password.
         $row = mysqli_fetch_assoc($result);
         $_SESSION["email"] = $row["email"];
         $_SESSION["role"] = $row["role"];
